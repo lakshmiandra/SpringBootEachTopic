eaea404b001f2f99b4a650948116266e4507ae4b
Lakshminarayana Andra <lakshminarayana.andra@ascenaretail.com>
2020-04-20 00:30:05 +0530

    DI

---
+++ spring-bean-dependency-injection/src/main/java/com/lakshmi/demo/springbeandependencyinjection/DemoApplication.java
@@ -0,0 +1,37 @@
+package com.lakshmi.demo.springbeandependencyinjection;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements ApplicationRunner {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		System.out.println("Hello World from application runner");
+	}
+	
+}
+/*public class RestfulWebServicesApplication implements CommandLineRunner {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("Hello world from Command Line Runner");
+	}
+}*/
+
